b75b29a2083957d20a961042e9ac7bd59257cc6f
Dennis Tomas <dtomas@onegreenbyte.com>
2015-03-20 18:21:56 +0100

    Added ManagedTray.refresh().

--- traylib/managed_tray.py
+++ traylib/managed_tray.py
@@ -10,10 +10,38 @@ class ManagedTray(Tray):
                  create_menu_icon=MenuIcon):
         Tray.__init__(self, icon_config, tray_config, create_menu_icon)
         self.__managers = [manager(self) for manager in managers]
+        self.__blocked = False
 
-        for manage, _unmanage in self.__managers:
-            tasks.Task(manage())
+        def _manage():
+            while self.__blocked:
+                yield None
+            self.__blocked = True
+            for manage, _unmanage in self.__managers:
+                for x in manage():
+                    yield None
+            self.__blocked = False
+        tasks.Task(_manage())
 
     def quit(self):
-        for _manage, unmanage in self.__managers:
-            tasks.Task(unmanage())
+        def _unmanage():
+            while self.__blocked:
+                yield None
+            self.__blocked = True
+            for _manage, unmanage in self.__managers:
+                for x in unmanage():
+                    yield None
+            self.__blocked = False
+        tasks.Task(_unmanage())
+
+    def refresh(self):
+        def _refresh():
+            while self.__blocked:
+                yield None
+            self.__blocked = True
+            for manage, unmanage in self.__managers:
+                for x in unmanage():
+                    yield None
+                for x in manage():
+                    yield None
+            self.__blocked = False
+        tasks.Task(_refresh())
