3e9764d4e144247c80f922dceefcf9cfcb1efadf
Dennis Tomas <dtomas@onegreenbyte.com>
2016-02-15 23:16:01 +0100

    Default values for WinIconConfig attributes.

--- traylib/winicon_config.py
+++ traylib/winicon_config.py
@@ -2,13 +2,13 @@ from traylib.config import Config, Attribute
 
 
 class WinIconConfig(Config):
-    all_workspaces = Attribute('update_option_all_workspaces')
+    all_workspaces = Attribute(default=False)
     """
     C{True}, if windows on all workspaces should be visible in the associated
     L{WinIcon}s' menus.
     """
 
-    arrow = Attribute('update_option_arrow')
+    arrow = Attribute(default=True)
     """
     C{True}, if the L{WinIcon}s associated with the C{WinIconConfig} should
     have an arrow on them when they have more than one visible window.
